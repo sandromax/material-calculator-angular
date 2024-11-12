d2cb1d22014652ecc03bf9011b8c7b9e2b8aa0f4
sandromax <sandrondk@gmail.com>
2024-11-12 09:51:41 +0100

    adding translate modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './translate-loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    },
+    TranslateService
+  ]
 })
 export class AppComponent {
   title = 'calculatorAngular4';
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('ro');
+    this.translate.use('ro');
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
