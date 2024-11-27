c949d3183bed6f2bf34ca2793a081f51f102ad38
sandromax <sandrondk@gmail.com>
2024-11-27 14:38:54 +0100

    adding NGX-translate modules. closes #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import {LanguageSwitcherComponent} from './language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculatorAngular4';
+
+  constructor(private translate: TranslateService) {
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,32 @@ import { provideRouter } from '@angular/router';
 // import { routes } from './app/app-routing.module';
 import { routes } from './app/app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {importProvidersFrom} from '@angular/core';
+
+export function httpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(), // Підключення HttpClient для всього застосунку
-    provideRouter(routes), provideAnimationsAsync()
+    provideRouter(routes), provideAnimationsAsync(),
+    provideAnimations(),
+    {
+      provide: TranslateLoader,
+      useFactory: httpLoaderFactory,
+      deps: [HttpClient]
+    },
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }))
   ]
 }).catch(err => console.error(err));
